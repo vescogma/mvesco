dfc4b819e268ce9e95a5ad4bda7c1d89f183459a
Miguel Vesco <vescogma@gmail.com>
2016-02-08 22:22:35 -0500

    more themeing process

--- src/components/sections/Header.js
+++ src/components/sections/Header.js
@@ -11,7 +11,7 @@ const Header = () => {
         </div>
       </div>
       <div className="card-container">
-        <div className="fit shadow pb3 bg-white">
+        <div className="card-wrap">
           <div className="header-card col-12 sm-col-8">
             <div className="p2 image-wrap mx-auto">
               <img className="circle image-max" src="assets/me-small.jpg" />
