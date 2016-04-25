4f310867c284a45b78ffbecf7720595872d5f2d0
Miguel Vesco <vescogma@gmail.com>
2016-04-25 17:15:03 -0400

    some tiny bugs

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -27,61 +27,61 @@ export const headerProps = [
 ];
 
 export const projectsProps = [
-    {
-      label: 'Mandelbrot Final Project',
-      icon: 'mandelbrot',
-    },
-    {
-      label: 'Miniature Car Body Design',
-      icon: 'carbody',
-    },
-    {
-      label: 'Real-Time Motor Control',
-      icon: 'pid',
-    },
-    {
-      label: 'Automated Pill Dispenser',
-      icon: 'pillsafe',
-    },
-    {
-      label: 'Embedded Pacemaker & Monitor',
-      icon: 'pacemaker',
-    },
-    {
-      label: 'Skylight Reflector Heliostat',
-      icon: 'heliostat',
-    },
-  ];
+  {
+    label: 'Mandelbrot Final Project',
+    icon: 'mandelbrot',
+  },
+  {
+    label: 'Miniature Car Body Design',
+    icon: 'carbody',
+  },
+  {
+    label: 'Real-Time Motor Control',
+    icon: 'pid',
+  },
+  {
+    label: 'Automated Pill Dispenser',
+    icon: 'pillsafe',
+  },
+  {
+    label: 'Embedded Pacemaker & Monitor',
+    icon: 'pacemaker',
+  },
+  {
+    label: 'Skylight Reflector Heliostat',
+    icon: 'heliostat',
+  },
+];
 
 export const contactProps = [
-    {
-      label: 'Email',
-      icon: 'email',
-      href: 'mailto:vescogma@gmail.com',
-    },
-    {
-      label: 'LinkedIn',
-      icon: 'linkedin',
-      href: 'https://ca.linkedin.com/in/miguelvesco',
-    },
-    {
-      label: 'GitHub',
-      icon: 'github',
-      href: 'https://github.com/vescogma',
-    },
-    {
-      label: 'Facebook',
-      icon: 'facebook',
-      href: 'https://facebook.com/miguel.vesco',
-    },
-    {
-      label: 'Google',
-      icon: 'google',
-      href: 'https://plus.google.com/+MiguelVesco',
-    },
-    {
-      label: 'Twitter',
-      icon: 'twitter',
-      href: 'https://twitter.com/@WhyMiguelWhy',
-    },
-  ];
+  {
+    label: 'Email',
+    icon: 'email',
+    href: 'mailto:vescogma@gmail.com',
+  },
+  {
+    label: 'LinkedIn',
+    icon: 'linkedin',
+    href: 'https://ca.linkedin.com/in/miguelvesco',
+  },
+  {
+    label: 'GitHub',
+    icon: 'github',
+    href: 'https://github.com/vescogma',
+  },
+  {
+    label: 'Facebook',
+    icon: 'facebook',
+    href: 'https://facebook.com/miguel.vesco',
+  },
+  {
+    label: 'Google',
+    icon: 'google',
+    href: 'https://plus.google.com/+MiguelVesco',
+  },
+  {
+    label: 'Twitter',
+    icon: 'twitter',
+    href: 'https://twitter.com/@WhyMiguelWhy',
+  },
+];
